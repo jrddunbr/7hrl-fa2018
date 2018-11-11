2cfe0673f638df112adb6464bc89ebe6f9b2c9d6
Jared Dunbar <jrddunbr@gmail.com>
2018-11-11 14:11:37 -0500

    doot, doot! got some rendering

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ class Color(enum.IntEnum):
     TAN = 15
 
 class Gui:
-    def __init__(self):
-        pass
+    def __init__(self, game):
+        self.game = game
 
     # render a chatbox to the screen
     def chatbox(self, x, y, text, carrot=False):
@@ -67,26 +67,65 @@ class Gui:
 
         return x + 3, lowest + 4 # two pixels drectly below text area carrot
 
+    def renderCursor(self, design=0, color=Color.LIGHT_GRAY):
+        if design == 1:
+            pyxel.pix(pyxel.mouse_x - 1, pyxel.mouse_y, color)
+            pyxel.pix(pyxel.mouse_x + 1, pyxel.mouse_y, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y - 1, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y + 1, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y, color)
+            self.chatbox(pyxel.mouse_x + 2, pyxel.mouse_y + 2, "({},{})".format(pyxel.mouse_x, pyxel.mouse_y))
+        else:
+            pyxel.pix(pyxel.mouse_x - 1, pyxel.mouse_y, color)
+            pyxel.pix(pyxel.mouse_x + 1, pyxel.mouse_y, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y - 1, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y + 1, color)
+            pyxel.pix(pyxel.mouse_x, pyxel.mouse_y, color)
+
+    def drawGrid(self, color=Color.LIGHT_GRAY):
+        # draw tiles
+        for x in range(self.game.tileX):
+            pyxel.line((x * self.game.TILESIZE), 0, self.game.TILESIZE * x, self.game.TILESIZE * self.game.tileY, Color.DARK_GRAY)
+        for y in range(self.game.tileY):
+            pyxel.line(0, (y * self.game.TILESIZE), self.game.TILESIZE * self.game.tileY, self.game.TILESIZE * y, Color.DARK_GRAY)
+
 
 class Game:
     def __init__(self):
-        pyxel.init(200, 160)
-        pyxel.mouse(True)
-        self.gui = Gui()
+
+        self.TILESIZE = 16
+
+        self.tileX = 15
+        self.tileY = 15
+
+        pyxel.init(self.TILESIZE * self.tileX, self.TILESIZE * self.tileY)
+        #pyxel.mouse(True)
+        self.gui = Gui(self)
+        self.renderGrid = True
         pyxel.run(self.update, self.draw)
 
     def update(self):
         # do game updates
-        pass
+        self.renderGrid = not self.renderGrid
 
     def draw(self):
         # clear the screen
         pyxel.cls(0)
+
         # draw things
+
+        # draw 16x16 square for reference
+        pyxel.rect(16, 16, 31, 31, Color.LIGHT_PURPLE)
+
+        # some test chatboxes
         self.gui.chatbox(20, 20, "hello, welcome to HVAC")
         self.gui.chatbox(20, 40, "hello,\nwelcome to HVAC")
         self.gui.chatbox(20, 60, "hello,\nwelcome to hvac,\nglad you could make it")
         x, y = self.gui.chatbox(20, 85, "1\n 2 \n 3\n4", carrot=True)
         pyxel.pix(x, y, Color.GREEN)
+
+        # render mouse cursor
+        self.gui.renderCursor(design=1)
+
 
 Game()
